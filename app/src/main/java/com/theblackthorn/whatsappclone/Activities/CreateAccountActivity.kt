57165dad9dc1311f30e396ed6ad279e24f422254
The Blackthorn <emekaujah@hotmail.co.uk>
2019-05-03 16:54:52 +1000

    Initial commit

---
+++ app/src/main/java/com/theblackthorn/whatsappclone/Activities/CreateAccountActivity.kt
@@ -0,0 +1,13 @@
+package com.theblackthorn.whatsappclone.Activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.theblackthorn.whatsappclone.R
+
+class CreateAccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+}
